04b855e3457ae04c0ba8c3f388b107ab2e67bd6d
Shubham Singh <shubham.tapmod@gmail.com>
2024-05-19 21:56:46 +0530

    Scene setup for button calls and binding

--- Assets/Script/SystemMonitorPlugin.cs
+++ Assets/Script/SystemMonitorPlugin.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+using UnityEngine.UI;
 
 public class SystemMonitorPlugin : MonoBehaviour
 {
+    [SerializeField] private Button startBtn;
+    [SerializeField] private Button stopBtn;
+
     [DllImport("__Internal")]
     private static extern void _startTracking();
 
     [DllImport("__Internal")]
     private static extern string _stopTracking();
+
+    public void Awake()
+    {
+        startBtn.onClick.AddListener(StartTracking);
+        stopBtn.onClick.AddListener(StopTracking);
+    }
 
     public void StartTracking()
     {
