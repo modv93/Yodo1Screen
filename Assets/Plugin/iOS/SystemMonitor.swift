97c64064075cf2e3cb24eb47b13e05a03d041ada
Shubham Singh <shubham.tapmod@gmail.com>
2024-05-21 00:45:27 +0530

    Statistics changed to min, max, avg

--- Assets/Plugin/iOS/SystemMonitor.swift
+++ Assets/Plugin/iOS/SystemMonitor.swift
@@ -14,10 +14,14 @@ import Metal
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(trackUsage), userInfo: nil, repeats: true)
     }
 
-    @objc public func stopTracking() -> [String: [Double]] {
+    @objc public func stopTracking() -> [String: [String: Double]] {
         timer?.invalidate()
         timer = nil
-        return ["cpu": cpuUsage, "gpu": gpuUsage, "ram": ramUsage]
+        return [
+            "cpu": calculateStats(for: cpuUsage),
+            "gpu": calculateStats(for: gpuUsage),
+            "ram": calculateStats(for: ramUsage)
+        ]
     }
 
     @objc private func trackUsage() {
@@ -28,18 +32,48 @@ import Metal
 
     private func getCPUUsage() -> Double {
         var kr: kern_return_t
-        var task_info_count: mach_msg_type_number_t
-        var tinfo = task_info_t.allocate(capacity: Int(TASK_INFO_MAX))
-        task_info_count = mach_msg_type_number_t(TASK_INFO_MAX)
-        kr = task_info(mach_task_self_, task_flavor_t(TASK_THREAD_TIMES_INFO), tinfo, &task_info_count)
+        var task_info_count: mach_msg_type_number_t = mach_msg_type_number_t(TASK_INFO_MAX)
+        var tinfo = task_info_t.allocate(capacity: Int(task_info_count))
+        defer { tinfo.deallocate() }
+
+        kr = task_info(mach_task_self_, task_flavor_t(TASK_BASIC_INFO), tinfo, &task_info_count)
         if kr != KERN_SUCCESS {
-            return -1
+            return -1.0
         }
 
-        let taskInfo = tinfo.withMemoryRebound(to: task_thread_times_info.self, capacity: 1) { $0.pointee }
-        let totalCPUTime = taskInfo.user_time.seconds + taskInfo.system_time.seconds
-        let cpuUsage = Double(totalCPUTime) * 100.0
-        return cpuUsage
+        let task_basic_info_ptr = tinfo.withMemoryRebound(to: task_basic_info.self, capacity: 1) { $0 }
+        let basic_info = task_basic_info_ptr.pointee
+
+        var thread_list: thread_act_array_t?
+        var thread_count: mach_msg_type_number_t = 0
+        kr = task_threads(mach_task_self_, &thread_list, &thread_count)
+        if kr != KERN_SUCCESS {
+            return -1.0
+        }
+
+        var tot_cpu: Double = 0
+        if let thread_list = thread_list {
+            for i in 0..<thread_count {
+                var thread_info_count: mach_msg_type_number_t = mach_msg_type_number_t(THREAD_INFO_MAX)
+                var thinfo = thread_info_t.allocate(capacity: Int(thread_info_count))
+                defer { thinfo.deallocate() }
+
+                kr = thread_info(thread_list[Int(i)], thread_flavor_t(THREAD_BASIC_INFO), thinfo, &thread_info_count)
+                if kr != KERN_SUCCESS {
+                    return -1.0
+                }
+
+                let thread_basic_info_ptr = thinfo.withMemoryRebound(to: thread_basic_info.self, capacity: 1) { $0 }
+                let thread_basic_info = thread_basic_info_ptr.pointee
+
+                if thread_basic_info.flags & TH_FLAGS_IDLE == 0 {
+                    tot_cpu += Double(thread_basic_info.cpu_usage) / Double(TH_USAGE_SCALE) * 100.0
+                }
+            }
+            vm_deallocate(mach_task_self_, vm_address_t(bitPattern: thread_list), vm_size_t(Int(thread_count) * MemoryLayout<thread_t>.stride))
+        }
+
+        return tot_cpu
     }
 
     private func getGPUUsage() -> Double {
@@ -60,30 +94,35 @@ import Metal
 
         let gpuUsageTime = end - start
         let totalElapsedTime = 1.0 // Assume 1 second interval for tracking
-        
-        // Convert to percentage
+
         let gpuUsagePercentage = (gpuUsageTime / totalElapsedTime) * 100.0
         return gpuUsagePercentage
     }
 
     private func getRAMUsage() -> Double {
-        let usedMemory = report_memory()
-        return Double(usedMemory)
-    }
-
-    private func report_memory() -> UInt64 {
-        var taskInfo = mach_task_basic_info()
-        var count = mach_msg_type_number_t(MemoryLayout.size(ofValue: taskInfo) / MemoryLayout<Int32>.size)
-        let kerr: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
-            $0.withMemoryRebound(to: integer_t.self, capacity: Int(count)) {
+        var info = mach_task_basic_info()
+        var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size) / 4
+        let kerr: kern_return_t = withUnsafeMutablePointer(to: &info) {
+            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
                 task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
             }
         }
 
         if kerr == KERN_SUCCESS {
-            return taskInfo.resident_size
+            return Double(info.resident_size)
         } else {
-            return 0
+            return -1
         }
     }
+
+    private func calculateStats(for data: [Double]) -> [String: Double] {
+        guard !data.isEmpty else { return ["min": 0, "max": 0, "avg": 0] }
+
+        let min = data.min() ?? 0
+        let max = data.max() ?? 0
+        let avg = data.reduce(0, +) / Double(data.count)
+
+        return ["min": min, "max": max, "avg": avg]
+    }
 }
+
--- Assets/Script/SystemMonitorPlugin.cs
+++ Assets/Script/SystemMonitorPlugin.cs
@@ -10,7 +10,8 @@ public class SystemMonitorPlugin : MonoBehaviour
     [SerializeField] private Button startBtn;
     [SerializeField] private Button stopBtn;
     [SerializeField] private TextMeshProUGUI output;
-
+    [SerializeField] private TextMeshProUGUI error;
+    private bool bStartLock;
     [DllImport("__Internal")]
     private static extern void _startTracking();
 
@@ -21,13 +22,22 @@ public class SystemMonitorPlugin : MonoBehaviour
     {
         startBtn.onClick.AddListener(StartTracking);
         stopBtn.onClick.AddListener(StopTracking);
+        bStartLock = false;
+        error.gameObject.SetActive(false);
     }
 
     public void StartTracking()
     {
+        if (error.gameObject.activeSelf) return;
+        if(bStartLock)
+        {
+            StartCoroutine(ShowError());
+            return;
+        }
         if (Application.platform == RuntimePlatform.IPhonePlayer)
         {
             _startTracking();
+            bStartLock = true;
         }
     }
 
@@ -37,8 +47,44 @@ public class SystemMonitorPlugin : MonoBehaviour
         {
             System.IntPtr resultPtr = _stopTracking();
             string result = Marshal.PtrToStringUTF8(resultPtr);
-            Debug.Log(result);
-            output.text = result;
+            ProcessResult(result);
         }
+    }
+
+    private void ProcessResult(string jsonResult)
+    {
+        var resultDict = JsonUtility.FromJson<UsageResult>(jsonResult);
+
+        Debug.Log($"CPU - Min: {resultDict.cpu.min}, Max: {resultDict.cpu.max}, Avg: {resultDict.cpu.avg}");
+        Debug.Log($"GPU - Min: {resultDict.gpu.min}, Max: {resultDict.gpu.max}, Avg: {resultDict.gpu.avg}");
+        Debug.Log($"RAM - Min: {resultDict.ram.min}, Max: {resultDict.ram.max}, Avg: {resultDict.ram.avg}");
+
+        output.text = $"CPU - Min: {resultDict.cpu.min}, Max: {resultDict.cpu.max}, Avg: {resultDict.cpu.avg} \n" +
+            ($"GPU - Min: {resultDict.gpu.min}, Max: {resultDict.gpu.max}, Avg: {resultDict.gpu.avg} \n ") +
+            ($"RAM - Min: {resultDict.ram.min}, Max: {resultDict.ram.max}, Avg: {resultDict.ram.avg} \n ");
+
+    }
+    IEnumerator ShowError()
+    {
+        error.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        error.gameObject.SetActive(false);
+
+    }
+
+    [System.Serializable]
+    private class UsageStats
+    {
+        public double min;
+        public double max;
+        public double avg;
+    }
+
+    [System.Serializable]
+    private class UsageResult
+    {
+        public UsageStats cpu;
+        public UsageStats gpu;
+        public UsageStats ram;
     }
 }
