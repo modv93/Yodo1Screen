05f699b90500e444139f37bc7ff4482e6e80fcca
Shubham Singh <shubham.tapmod@gmail.com>
2024-05-21 01:03:02 +0530

    Removed warning and changed interactibility of buttons and changed output format

--- Assets/Script/SystemMonitorPlugin.cs
+++ Assets/Script/SystemMonitorPlugin.cs
@@ -10,8 +10,6 @@ public class SystemMonitorPlugin : MonoBehaviour
     [SerializeField] private Button startBtn;
     [SerializeField] private Button stopBtn;
     [SerializeField] private TextMeshProUGUI output;
-    [SerializeField] private TextMeshProUGUI error;
-    private bool bStartLock;
     [DllImport("__Internal")]
     private static extern void _startTracking();
 
@@ -22,22 +20,17 @@ public class SystemMonitorPlugin : MonoBehaviour
     {
         startBtn.onClick.AddListener(StartTracking);
         stopBtn.onClick.AddListener(StopTracking);
-        bStartLock = false;
-        error.gameObject.SetActive(false);
+        startBtn.interactable = true;
+        stopBtn.interactable = false;
     }
 
     public void StartTracking()
     {
-        if (error.gameObject.activeSelf) return;
-        if(bStartLock)
-        {
-            StartCoroutine(ShowError());
-            return;
-        }
         if (Application.platform == RuntimePlatform.IPhonePlayer)
         {
+            startBtn.interactable = false;
+            stopBtn.interactable = true;
             _startTracking();
-            bStartLock = true;
         }
     }
 
@@ -45,6 +38,8 @@ public class SystemMonitorPlugin : MonoBehaviour
     {
         if (Application.platform == RuntimePlatform.IPhonePlayer)
         {
+            stopBtn.interactable = false;
+            startBtn.interactable = true;
             System.IntPtr resultPtr = _stopTracking();
             string result = Marshal.PtrToStringUTF8(resultPtr);
             ProcessResult(result);
@@ -55,20 +50,13 @@ public class SystemMonitorPlugin : MonoBehaviour
     {
         var resultDict = JsonUtility.FromJson<UsageResult>(jsonResult);
 
-        Debug.Log($"CPU - Min: {resultDict.cpu.min}, Max: {resultDict.cpu.max}, Avg: {resultDict.cpu.avg}");
-        Debug.Log($"GPU - Min: {resultDict.gpu.min}, Max: {resultDict.gpu.max}, Avg: {resultDict.gpu.avg}");
-        Debug.Log($"RAM - Min: {resultDict.ram.min}, Max: {resultDict.ram.max}, Avg: {resultDict.ram.avg}");
+        Debug.Log($"CPU - Min: {resultDict.cpu.min}%, Max: {resultDict.cpu.max}%, Avg: {resultDict.cpu.avg}% \n");
+        Debug.Log($"GPU - Min: {resultDict.gpu.min}%, Max: {resultDict.gpu.max}%, Avg: {resultDict.gpu.avg}% \n");
+        Debug.Log($"RAM - Min: {resultDict.ram.min} MB, Max: {resultDict.ram.max} MB, Avg: {resultDict.ram.avg} MB \n");
 
-        output.text = $"CPU - Min: {resultDict.cpu.min}, Max: {resultDict.cpu.max}, Avg: {resultDict.cpu.avg} \n" +
-            ($"GPU - Min: {resultDict.gpu.min}, Max: {resultDict.gpu.max}, Avg: {resultDict.gpu.avg} \n ") +
-            ($"RAM - Min: {resultDict.ram.min}, Max: {resultDict.ram.max}, Avg: {resultDict.ram.avg} \n ");
-
-    }
-    IEnumerator ShowError()
-    {
-        error.gameObject.SetActive(true);
-        yield return new WaitForSeconds(2);
-        error.gameObject.SetActive(false);
+        output.text = $"CPU - Min: {resultDict.cpu.min}%, Max: {resultDict.cpu.max}%, Avg: {resultDict.cpu.avg}% \n" +
+            ($"GPU - Min: {resultDict.gpu.min}%, Max: {resultDict.gpu.max}%, Avg: {resultDict.gpu.avg}% \n") +
+            ($"RAM - Min: {resultDict.ram.min} MB, Max: {resultDict.ram.max} MB, Avg: {resultDict.ram.avg} MB \n");
 
     }
 
